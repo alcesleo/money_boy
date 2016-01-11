08c3ab937d3a245c89a217752a0a1948e6eab058
Jimmy Börjesson <alcesleo@gmail.com>
2016-01-11 23:59:22 +0100

    Extract let-binding in converter_spec

--- spec/lib/money_boy/converter_spec.rb
+++ spec/lib/money_boy/converter_spec.rb
@@ -4,23 +4,21 @@ require 'money_boy/money'
 module MoneyBoy
   describe Converter do
     let(:conversion_rates) { { 'EUR' => { 'USD' => 1.11 } } }
+    let(:converter)        { Converter.new(conversion_rates) }
 
     it 'converts a Money object to a known currency' do
-      converter = Converter.new(conversion_rates)
       fifty_euro = Money.new(50, 'EUR')
 
       expect(converter.convert(fifty_euro, 'USD')).to eq Money.new(55.5, 'USD')
     end
 
     it 'raises an error if the target currency rate is unknown' do
-      converter = Converter.new(conversion_rates)
       fifty_euro = Money.new(50, 'EUR')
 
       expect { converter.convert(fifty_euro, 'Bitcoin') }.to raise_exception(ConversionError, /Unknown exchange rate/)
     end
 
     it 'raises an error if the base currency rate is unknown' do
-      converter = Converter.new(conversion_rates)
       fifty_btc = Money.new(50, 'Bitcoin')
 
       expect { converter.convert(fifty_btc, 'EUR') }.to raise_exception(ConversionError, /Unknown exchange rate/)
