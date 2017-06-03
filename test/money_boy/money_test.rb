955f2b05bbd41610da6a99f8199d0f011471708c
Jimmy Börjesson <alcesleo@gmail.com>
2017-06-03 12:53:50 +0200

    Refactor test

--- test/money_boy/money_test.rb
+++ test/money_boy/money_test.rb
@@ -7,15 +7,18 @@ class MoneyTest < Minitest::Test
   def test_equality
     assert_equal Money.new(9, "EUR"), Money.new(9, "EUR")
     assert_equal Money.new(9, "EUR"), Money.new(9.0, "EUR")
-    assert_equal Money.new(9, "EUR"), Money.new(9.0, "EUR")
 
     refute_equal Money.new(9, "EUR"), Money.new(10, "EUR")
     refute_equal Money.new(9, "EUR"), Money.new(9, "USD")
     refute_equal Money.new(9, "EUR"), Object.new
     refute_equal Money.new(9, "EUR"), nil
+  end
 
+  def test_eql
     assert Money.new(9, "EUR").eql?(Money.new(9, "EUR")), "Expected #eql? to do the same as #=="
+  end
 
+  def test_hash
     assert_equal Money.new(9, "EUR").hash, Money.new(9, "EUR").hash
   end
 end
