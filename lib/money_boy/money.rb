c3295002d92ab41c9f5b19ccf12dd8545d9004c2
Jimmy Börjesson <alcesleo@gmail.com>
2017-06-03 17:20:25 +0200

    Add conversion methods

--- lib/money_boy/money.rb
+++ lib/money_boy/money.rb
@@ -16,6 +16,7 @@ module MoneyBoy
 
     def self.conversion_rates=(conversion_rates)
       @conversion_rates = conversion_rates
+      define_conversion_methods
       define_convenience_constructors unless @disable_monkey_patches
     end
 
@@ -82,18 +83,32 @@ module MoneyBoy
       end
     end
 
+    def self.define_conversion_methods
+      currencies.each do |currency|
+        define_method("to_#{currency.downcase}") do
+          convert_to(currency)
+        end
+      end
+    end
+    private_class_method :define_conversion_methods
+
     def self.define_convenience_constructors
       money_class = self
-      @conversion_rates.flat_map { |base, rates| [base] + rates.keys }.each do |currency|
+      currencies.each do |currency|
         next if Numeric.method_defined?(currency.downcase)
 
         Numeric.class_eval do
-          define_method currency.downcase do
+          define_method(currency.downcase) do
             money_class.new(self, currency)
           end
         end
       end
     end
     private_class_method :define_convenience_constructors
+
+    def self.currencies
+      @conversion_rates.flat_map { |base, rates| [base] + rates.keys }
+    end
+    private_class_method :currencies
   end
 end
